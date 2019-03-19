077be6e37c1991563c4a9c389cd681cf7b65490b
Camilo Mora <kalufau@hotmail.com>
2019-03-19 17:56:16 -0500

    Clases helper usuarios

---
+++ Shop.Web/Helper/IUserHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace Shop.Web.Helper
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Identity;
+    using Shop.Web.Data.Entities;
+
+    public interface IUserHelper
+    {
+        Task<User> GetUserByEmailAsync(string email);
+        Task<IdentityResult> AddUserAsync(User user, string password);
+    }
+}
